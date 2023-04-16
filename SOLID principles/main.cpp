a0a5632f099efd49d14d5b5644a423762a142042
Milad Rasouli <106727848+Milad75Rasouli@users.noreply.github.com>
2023-04-16 16:28:03 +0330

    After OCP.

--- SOLID principles/main.cpp
+++ SOLID principles/main.cpp
@@ -5,7 +5,7 @@
 *   SOLID
 *   =========================
 *   S - SRP - Single Responsibility Principle = A class should have one, and only one, reson to change.
-*   O - OCP - Open/Closed Principle = Entities should be open for extension, but closed for modification. Uncle Bob says:
+*   O - OCP - Open/Closed Principle = Entities should be open for extension, but closed for modification.
 *   L - LCP - Liskov Substitution Principle
 *   I - ISP - Interface Segregation Principle
 *   D - DTP - Dependency Inversion Principle
@@ -16,27 +16,34 @@ class Receipt
 
 };
 
+// Contract
+class PaymentInterface {
+public:
+    virtual auto acceptPayment() -> void = 0;
+};
+
+class BitcoinPaymentMethod :public PaymentInterface {
+public:
+    auto acceptPayment()->void override {
+        // logic to accept bitcoin
+    }
+};
+class CreditCardPaymentMethod :public PaymentInterface {
+public:
+    auto acceptPayment()->void override {
+        // logic to accept creditcard
+    }
+};
+
+// Uncle Bob says: Seperate extensible behavior behind an interface, and flip the dependencies.
 class Payment
 {
+    PaymentInterface* _payment;
 public:
-    auto begin(Receipt receipt)->void 
+    auto begin(PaymentInterface&& payment)->void
     {
-        this->acceptCash(receipt);
+        payment.acceptPayment();
     }
-
-    auto acceptCash(Receipt& receipt)->void
-    {
-        // accept the cash.
-    }
-    auto card(Receipt& receipt)->void
-    {
-        // accept the cash.
-    }
-    auto paypal(Receipt& receipt)->void
-    {
-        // accept the cash.
-    }
-
 };
 
 int main()
