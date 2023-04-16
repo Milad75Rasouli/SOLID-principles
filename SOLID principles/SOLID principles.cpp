09c7346610b4074a86ccd3789e1db9f9a1365c3d
Milad Rasouli <106727848+Milad75Rasouli@users.noreply.github.com>
2023-04-16 14:50:50 +0330

    before SRP

--- SOLID principles/SOLID principles.cpp
+++ SOLID principles/SOLID principles.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 
+
+/*
+*   SOLID
+*   =========================
+*   S - SRP - Single Responsibility Principle
+*   O - OCP - Open/Closed Principle
+*   L - LCP - Liskov Substitution Principle
+*   I - ISP - Interface Segregation Principle
+*   D - DTP - Dependency Inversion Principle
+*/
+
+class User
+{
+public:
+    auto create(int id)->void {
+		try
+		{
+			//save
+		}
+		catch (const std::exception& err)
+		{
+			this->log(err.what());
+		}
+    }
+
+	auto log(const char* msg)->void{
+		std::cerr << msg << std::endl;
+	}
+
+};
+
+
 int main()
 {
     std::cout << "Hello World!\n";
