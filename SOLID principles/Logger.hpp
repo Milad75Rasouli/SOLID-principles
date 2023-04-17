f612b8fac6d6a6c722c403bbec9093c3a791a6d9
Milad Rasouli <106727848+Milad75Rasouli@users.noreply.github.com>
2023-04-17 10:27:28 +0330

    Before LCP

--- SOLID principles/Logger.hpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#pragma once
-class Logger {
-
-public:
-	static auto log(const char* msg)->void {
-		std::cerr << msg << std::endl;
-	}
-};
--- SOLID principles/main.cpp
+++ SOLID principles/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "Logger.hpp"
 
 /*
 *   SOLID
@@ -11,49 +10,9 @@
 *   D - DTP - Dependency Inversion Principle
 */
 
-class Receipt 
-{
-
-};
-
-// Contract
-class PaymentInterface {
-public:
-    virtual auto acceptPayment() -> void = 0;
-};
-
-class BitcoinPaymentMethod :public PaymentInterface {
-public:
-    auto acceptPayment()->void override {
-        // logic to accept bitcoin
-        std::cout << "acceptPayment function has been called from BitcoinPaymentMethod\n";
-    }
-};
-class CreditCardPaymentMethod :public PaymentInterface {
-public:
-    auto acceptPayment()->void override {
-        // logic to accept creditcard
-        std::cout << "acceptPayment function has been called from CreditCardPaymentMethod\n";
-    }
-};
-
-// Uncle Bob says: Seperate extensible behavior behind an interface, and flip the dependencies.
-class Payment
-{
-    PaymentInterface* _payment;
-public:
-    auto begin(PaymentInterface& payment)->void
-    {
-        payment.acceptPayment();
-    }
-};
 
 int main()
 {
-    BitcoinPaymentMethod bitcoin;
-    CreditCardPaymentMethod card;
-    Payment pay;
-    pay.begin(card);
-    pay.begin(bitcoin);
+
     std::cout << "Hello World!\n";
 }
