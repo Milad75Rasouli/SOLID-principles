403cf000dd38c5443ce9008e6793987dbcc49a20
Milad Rasouli <106727848+Milad75Rasouli@users.noreply.github.com>
2023-04-17 10:47:27 +0330

    Before LCP

--- SOLID principles/main.cpp
+++ SOLID principles/main.cpp
@@ -5,14 +5,35 @@
 *   =========================
 *   S - SRP - Single Responsibility Principle = A class should have one, and only one, reson to change.
 *   O - OCP - Open/Closed Principle = Entities should be open for extension, but closed for modification.
-*   L - LCP - Liskov Substitution Principle
+*   L - LCP - Liskov Substitution Principle = Derived classes must be substitutable for their bas classes.
 *   I - ISP - Interface Segregation Principle
 *   D - DTP - Dependency Inversion Principle
 */
 
+// LCP - Liskov Substitution Principle 
+
+class Car {
+public:
+    std::string color{};
+    auto Drive(Car& car)->std::string // child must follow its parent. even the returnd paramer. like string
+    {
+        return "Please drive safely\n";
+    }
+};
+
+class Honda : public Car {
+public:
+    auto Drive(Car& car)->void
+    {
+        std::clog << "Drive has been called from Honda class\n";
+        if (this->color == "silver")
+        {
+            throw "silver is not available.";
+        }
+    }
+};
 
 int main()
 {
-
     std::cout << "Hello World!\n";
 }
